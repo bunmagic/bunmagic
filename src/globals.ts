14ced37c31cde59c259c7679a68455a276a0a2b7
Nauris Pūķis <hi@pyronaur.com>
2024-01-27 10:51:58 +0200

    Declare globals

--- src/globals.ts
+++ src/globals.ts
@@ -5,20 +5,16 @@ import fs from 'fs-extra';
 import { globby } from 'globby';
 import os from 'node:os';
 import path from 'path';
-import which from 'which';
 import minimist from 'minimist';
 
 
 const argv = minimist(process.argv.slice(2) || [])
 
-// Exporting all imported modules
-export default {
-	$,
-	chalk,
-	fs,
-	globby,
-	os,
-	path,
-	which,
-	argv,
-};
+// Exporting all imported modules individually
+export { $ };
+export { chalk };
+export { fs };
+export { globby };
+export { os };
+export { path };
+export { argv };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import globals from "./globals";
+import * as globals from "./globals";
 Object.assign(globalThis, globals);
+
+declare global {
+	const $: typeof globals.$;
+	const chalk: typeof globals.chalk;
+	const fs: typeof globals.fs;
+	const globby: typeof globals.globby;
+	const os: typeof globals.os;
+	const path: typeof globals.path;
+	const argv: typeof globals.argv;
+}
