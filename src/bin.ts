abae729326b86d03eb618124342c587e3241851b
Nauris Pūķis <hi@pyronaur.com>
2024-01-28 11:03:49 +0200

    buns to bin

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,38 @@
 import "./index";
-const commandFile = argv._.shift();
-if (!commandFile) {
+import { getCommands } from './buns/commands';
+import env_requirements from "./buns/env-requirements";
+
+
+if (true !== await env_requirements()) {
+	console.log(`Environment requirements not met.`);
+	process.exit(1);
+}
+
+const sourceDirectory = argv._.shift();
+if (!sourceDirectory) {
 	throw new Error(`No command file specified.`);
 }
-await import(commandFile);
+
+// Turn off verbose mode by default
+BUNS.verbose = argv.verbose || Bun.env.DEBUG || false;
+
+const files = (await $`ls ${sourceDirectory}`.text())
+	.split("\n")
+	.map((file: string) => file.trim())
+	.filter(Boolean)
+	.map(file => path.resolve(sourceDirectory, file));
+
+
+
+const { router, commands } = await getCommands(files);
+const input = argv._[0];
+
+try {
+	const command = commands.get(input);
+	if (command === undefined || command.type === "command") {
+		await router.run(command, commands);
+	}
+
+} catch (e) {
+	console.log(chalk.bold.red("Error: "), e);
+}
