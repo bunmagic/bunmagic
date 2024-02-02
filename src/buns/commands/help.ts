7083f83cfd828b1f055f3e6663b743169a501f95
Nauris Pūķis <hi@pyronaur.com>
2024-02-02 17:53:59 +0200

    dedupe commands

--- src/buns/commands/help.ts
+++ src/buns/commands/help.ts
@@ -5,19 +5,34 @@ export const desc = "Get the full list of available commands";
 export const usage = "buns help";
 
 export default async function run(commands: Map<string, Command>) {
-	const help = Array.from(commands.values()).map((command) => {
-		let output = `\n  `;
-		output += `${chalk.bold(command.name)}`
-		if (command.desc) {
-			output += ` - `
-			output += command.desc
-		}
-		output += `\n  `
-		output += chalk.gray(command.usage)
-		output += `\n`
 
-		return output;
-	}).join("");
+	let help = ``;
+	commands.forEach((command, name) => {
+		if (
+			command.type !== "command"
+			|| (command.alias && command.alias.includes(name))
+			|| command.name.startsWith("_")
+		) {
+			return;
+		}
+		help += `\n  `;
+		help += `${chalk.bold(command.name)} `
+		if (command.desc) {
+			help += ` - `
+			help += command.desc
+		}
+		if (command.alias) {
+			let alias = "";
+			alias += ` (alias: `
+			alias += command.alias.join(", ")
+			alias += `)`
+			help += chalk.dim(alias)
+
+		}
+		help += `\n  `
+		help += chalk.gray(command.usage)
+		help += `\n`
+	})
 
 	console.log(help);
 }
