a6ff9267daa550a1d7a382085645d175e9444b28
Nauris Pūķis <hi@pyronaur.com>
2024-02-03 11:53:01 +0200

    move bins to /bin/

--- src/bin/exec-namespace.ts
+++ src/bin/exec-namespace.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 import "bunshell";
-import { getCommands } from './lib/commands';
-import { getSources } from './lib/sources';
+import { getCommands } from '../lib/commands';
+import { getSources } from '../lib/sources';
 
 const namespace = argv._.shift();
 if (!namespace) {
