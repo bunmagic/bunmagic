87bbfb2a08b8f9affd8e2561a9b25322e0e07f50
Nauris Pūķis <hi@pyronaur.com>
2024-02-04 10:57:20 +0200

    Reuse `help` script in default router

--- src/lib/default-router.ts
+++ src/lib/default-router.ts
@@ -1,15 +1,25 @@
+import help from '../scripts/help';
 import type { RouterCallback } from './commands';
 
 
-const router: RouterCallback = async (script, command) => {
+const router: RouterCallback = async (script, command, commands) => {
+
+	const input = argv._.join(" ");
+	if (argv.h || input === "help" || !command) {
+		await help(commands);
+		return;
+	}
+
 	if (!command) {
 		console.log("No command found.");
 		return;
 	} else {
+
 		try {
 			await script();
 		} catch (e) {
-			console.log("Error running command: ", 'name' in command ? command.name : command.file);
+			console.log(e);
+			console.log("Error running command: ", (command && 'name' in command) ? command.name : command.file);
 		}
 	}
 }
--- src/scripts/help.ts
+++ src/scripts/help.ts
@@ -4,37 +4,48 @@ export const name = "help";
 export const desc = "Get the full list of available commands";
 export const usage = "bunshell help";
 
+
+function describeCommand(command: Command) {
+	let desc = `\n${ansis.bold(command.name)} `;
+	if (command.desc) {
+		desc += ` - `;
+		desc += command.desc;
+	}
+	if (command.alias) {
+		let alias = "";
+		alias += ` (alias: `
+		alias += command.alias.join(", ")
+		alias += `)`
+		desc += ansis.dim(alias);
+
+	}
+	if (command.usage) {
+		desc += `\n`;
+		desc += ansis.gray(command.usage);
+	}
+	return desc.split("\n").join("\n  ");
+}
+
+async function describeRawCommand(command: RawCommand) {
+	let desc = `\n${ansis.bold(command.name)}`;
+	return desc.split("\n").join("\n  ");
+}
+
 export default async function (commands: Map<string, Command | NotFound | RawCommand>) {
-	let help = ``;
-	commands.forEach((command, name) => {
-		if (
-			command.type !== "command"
-			|| (command.alias && command.alias.includes(name))
-			|| command.name.startsWith("_")
-		) {
-			return;
+	for (const [name, command] of commands.entries()) {
+		if (('alias' in command && command.alias && command.alias.includes(name))) {
+			continue;
 		}
-		help += `\n  `;
-		help += `${ansis.bold(command.name)} `
-		if (command.desc) {
-			help += ` - `
-			help += command.desc
+		if (Bun.file(command.file).name?.startsWith("_")) {
+			continue;
 		}
-		if (command.alias) {
-			let alias = "";
-			alias += ` (alias: `
-			alias += command.alias.join(", ")
-			alias += `)`
-			help += ansis.dim(alias)
-
-		}
-		if (command.usage) {
-			help += `\n  `
-			help += ansis.gray(command.usage)
-			help += `\n`
+		if (command.type === "command") {
+			console.log(describeCommand(command));
 		}
 
-	})
+		if (command.type === "raw-command") {
+			console.log(await describeRawCommand(command));
+		}
 
-	console.log(help);
+	}
 }
