7e440e55d3cc8c72cc104c472e330758f90c7281
Nauris Pūķis <hi@pyronaur.com>
2024-02-09 13:45:08 +0200

    Setup & Uninstall

--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -5,6 +5,7 @@ export const desc = "Install bunshell and set up your environment";
 export const usage = "bunshell install";
 
 
+
 async function require<T>(callback: () => Promise<T>, attempts = 3): Promise<T> {
 	while (attempts > 0) {
 		try {
@@ -21,12 +22,12 @@ async function require<T>(callback: () => Promise<T>, attempts = 3): Promise<T> 
 	throw die("Failed to complete operation. Exiting.");
 }
 
+function availableRcFiles() {
+	const rcFiles = [`${$HOME}/.bashrc`, `${$HOME}/.zshrc`, `${$HOME}/.profile`, `${$HOME}/.bash_profile`];
+	return Promise.all(rcFiles.filter(f => Bun.file(f).exists()));
+}
 async function setupBinPath(binPath: string) {
-	const bashrc = `${$HOME}/.bashrc`;
-	const zshrc = `${$HOME}/.zshrc`;
-	const profile = `${$HOME}/.profile`;
-	const bashProfile = `${$HOME}/.bash_profile`;
-	const rcFiles = await Promise.all([bashrc, zshrc, profile, bashProfile].filter(f => Bun.file(f).exists()));
+	const rcFiles = await availableRcFiles();
 
 	const select = [...rcFiles, "Custom"];
 	let rcFile = selection(select, `Which file would you like to add bunshell to?`);
@@ -83,9 +84,33 @@ async function setupConfig(bsPath: string) {
 	config.set(defaults);
 }
 
+export async function uninstall() {
+	const confirm = ack("Are you sure you want to uninstall bunshell?");
+	if (!confirm) {
+		return;
+	}
+	const rcFiles = await availableRcFiles();
+	const binPath = `${$HOME}/.bunshell/bin`;
+
+	for (const file of rcFiles) {
+		const content = await Bun.file(file).text();
+		if (content.includes(binPath)) {
+			await Bun.write(file, content.replace(`export PATH="${binPath}:$PATH"\n`, ""));
+			console.log(`\n- Removed ${binPath} from ${file}`);
+		}
+	}
+
+
+	cd($HOME);
+	await $`rm -rf ${$HOME}/.bunshell`;
+	die(`Uninstalled bunshell.`);
+}
 
 export default async function setup() {
 
+	if (argv.remove || argv.uninstall || argv._[0] === "remove" || argv._[0] === "uninstall") {
+		return await uninstall();
+	}
 	console.log(`\nInstalling ${ansis.bold("bunshell")}...\n`);
 	if (!Bun.env.PATH) {
 		throw die("Can't find $PATH variable. Exiting.");
@@ -109,7 +134,7 @@ export default async function setup() {
 
 	const bsAliasBin = `${binPath}/bs`;
 	const bsAliasQuestion = `\n- Do you want to setup ${ansis.bold("bs")} as a shortcut for ${ansis.bold("bunshell")}?`;
-	if (!await Bun.file(bsAliasBin).exists() && Bun.which("bs") === undefined && ack(bsAliasQuestion)) {
+	if (!await Bun.file(bsAliasBin).exists() && Bun.which("bs") === null && ack(bsAliasQuestion)) {
 		await Bun.write(`${bsAliasBin}`, `#!/bin/bash\nbunshell $@`);
 		await $`chmod +x ${bsAliasBin}`;
 		console.log(`\n- Created a new bin: ${ansis.bold("bs")} -> ${bsAliasBin} \n`);
@@ -119,4 +144,7 @@ export default async function setup() {
 
 	// Welcome!
 	await displayWelcomeMessage();
+
+
+	console.log(`\n- Open a new terminal to reload your ${ansis.bold("$PATH")} variable to apply the changes.`);
 }
