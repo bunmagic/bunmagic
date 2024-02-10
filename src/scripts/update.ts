4927226e333b74d3c528f9c82391b070ce216878
Nauris Pūķis <hi@pyronaur.com>
2024-02-10 12:41:42 +0200

    Improve updater

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -4,7 +4,12 @@ export const name = "update";
 export const desc = "Update bunism to the latest version";
 
 export default async function update() {
+	const cv = await version();
 	await $`bun update -g bunism`;
-	console.log("Updated bunism to the latest version.");
-	await version();
+	const nv = await version();
+	if (cv !== nv) {
+		console.log(`\nUpdated bunism from ${cv} to ${nv}`);
+	} else {
+		console.log(`\nYou are already using the latest version of bunism.`);
+	}
 }
