0f03e4201422ba6031b3a0695439592844ef7945
Nauris Pūķis <hi@pyronaur.com>
2024-02-23 14:21:34 +0200

    Configurable $

--- src/globals/index.ts
+++ src/globals/index.ts
@@ -1,18 +1,34 @@
 /* eslint-disable unicorn/prefer-export-from */
 // Importing each module
 import os from 'node:os';
-import {$} from 'bun';
 import ansis from 'ansis';
+import {$} from 'bun';
 import {notMinimist} from './not-minimist';
 
 export {default as path} from 'node:path';
 export {notMinimist};
 
+export const BM = {
+	verbose: false,
+} as const;
+
+/**
+ * Customize $ to allow enabling/disabling verbose mode globally
+ */
+Object.assign($, async (...properties: Parameters<typeof $>) => {
+	if (BM.verbose) {
+		return $(...properties);
+	}
+
+	return $(...properties).quiet();
+});
+
 export * from './utils';
 export * from './fs';
+
 
 export const argv = notMinimist(Bun.argv.slice(2) || []);
 export const $HOME = os.homedir();
 export {
-	$, ansis, ansis as chalk, os,
+	ansis, ansis as chalk, os, $,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import * as globals from './globals/index';
 
-const BUNS_GLOBAL = {
-	verbose: true,
-};
-
 Object.assign(globalThis, {
 	...globals,
-	BUNS: BUNS_GLOBAL,
 });
 
 declare global {
-	const BUNS: typeof BUNS_GLOBAL;
 	const $: typeof globals.$;
 	const ansis: typeof globals.ansis;
 	const path: typeof globals.path;
@@ -25,6 +19,7 @@ declare global {
 	const os: typeof globals.os;
 	const $HOME: typeof globals.$HOME;
 	const $get: typeof globals.$get;
+	const BM: typeof globals.BM;
 }
 
 const customGlobalsFile = `${$HOME}/.bunmagic/custom-globals.ts`;
