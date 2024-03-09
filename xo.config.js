e73ed6afbb2f50e7871df7f1cfc528377ceb08d7
Nauris Pūķis <hi@pyronaur.com>
2024-03-09 12:09:47 +0200

    switch is ugly as it is, no need to make it worse

--- xo.config.js
+++ xo.config.js
@@ -5,6 +5,7 @@ export default {
 	bracketSpacing: true,
 	rules: {
 		'unicorn/prefer-ternary': 0,
+		'unicorn/switch-case-braces': 0,
 		// Avoid converting backtick strings if they're used for escaping single or double quotes.
 		'quotes': [
 			'error',
