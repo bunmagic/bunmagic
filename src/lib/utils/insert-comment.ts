5d74ef4f1e09579ac018ff079472d5a93b60025b
Nauris Pūķis <hi@pyronaur.com>
2024-03-09 20:34:48 +0200

    Don't auto-align

--- src/lib/utils/insert-comment.ts
+++ src/lib/utils/insert-comment.ts
@@ -2,8 +2,8 @@ import * as parser from 'comment-parser';
 
 function transform(content: string) {
 	const updatedParsed = parser.parse(content);
-	const transform = parser.transforms.flow(parser.transforms.align(), parser.transforms.indent(0));
-	return parser.stringify(transform(updatedParsed[0]));
+	const transform = parser.transforms.flow(parser.transforms.indent(1));
+	return parser.stringify(transform(updatedParsed[0])).trim();
 }
 
 export async function insertCommentLine(contents: string, insert: string) {
@@ -12,13 +12,13 @@ export async function insertCommentLine(contents: string, insert: string) {
 		const content = lines[0].replace(/\/\*{2}(.*?)\*{1,2}\//i, '$1');
 		lines = [
 			'/**',
-			` * ${content}`,
-			` * ${insert}`,
+			` * ${content.trim()}`,
+			` * ${insert.trim()}`,
 			' */',
 		];
 	} else {
 		lines = lines.slice(0, -1);
-		lines.push(`* ${insert}`, '*/');
+		lines.push(`* ${insert.trim()}`, '*/');
 	}
 
 	return transform(lines.map(l => l.trim()).join('\n'));
