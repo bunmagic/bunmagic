5f6f2b325a98cc9531a162ef67cfad9b2dcec35f
Nauris Pūķis <hi@pyronaur.com>
2024-04-09 10:46:48 +0300

    Clean-up symlink cmd

--- src/scripts/symlink.ts
+++ src/scripts/symlink.ts
@@ -1,6 +1,6 @@
 /**
  * Symlink all your sources to the bunmagic root directory for easy editing.
- * @flag [--link-src] The directory to link the sources to. Default: ~/.bunmagic/sources
+ * @flag [--target] The directory to link the sources to. Default: ~/.bunmagic/sources
  * @flag [--remove] Remove the symlinks.
  */
 import { PATHS } from '@lib/config';
@@ -8,32 +8,36 @@ import { getSources } from '@lib/sources';
 
 async function linkSource(source: string, target: string) {
 	console.log(ansis.green.dim(` Linking: ${ansis.reset(source)}\n To:      ${ansis.reset(target)}\n`));
+	console.log(`ln -s ${source} ${target}`);
 	await $`ln -s ${source} ${target}`;
 }
 
 export default async function symlinkSources() {
-	const targetSource = flags['link-src'] && typeof flags['link-src'] === 'string'
-		? flags['link-src']
-		: `${$HOME}/.bunmagic/sources`;
+	const target = flags['target'] && typeof flags['target'] === 'string'
+		? resolveTilde(flags['target'])
+		: `${$HOME}/.bunmagic/sources/`;
+
+	if (!target.endsWith('/')) {
+		throw new Error(`The target path must end with a trailing slash: ${target}`);
+	}
 
 	if (flags.remove) {
-		if (ack(`Remove ${targetSource}?`)) {
-			await $`rm -rf ${targetSource}`;
+		if (ack(`Remove ${target}?`)) {
+			await $`rm -rf ${target}`;
 		}
 		return;
 	}
 	const sources = await getSources();
 	const dirs = sources.map((source) => source.dir);
-	await ensureDirectory(targetSource);
+	await ensureDirectory(target);
 
 	for (const source of dirs) {
 		console.log(source);
-		const dirName = path.basename(source);
 		if (source.startsWith(PATHS.bunmagic)) {
 			console.log(ansis.yellow.dim(` Skipping: ${ansis.reset(source)}\n`));
 			continue;
 		}
-		await linkSource(source, `${targetSource}/${dirName}`);
+		await linkSource(source, target);
 	}
 
 }
