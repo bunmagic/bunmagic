85106290da52c363567c9c80099d3300b8a409e4
Nauris Pūķis <hi@pyronaur.com>
2024-04-25 09:14:54 +0300

    1.2.1

--- types/globals/selection.d.ts
+++ types/globals/selection.d.ts
@@ -1,4 +1,7 @@
-export declare function select<T extends string>(message: string, options: T[]): Promise<T>;
+export declare function select<T extends string>(message: string, options: T[] | {
+    value: T;
+    label: string;
+}[]): Promise<T>;
 export declare function autoselect<T extends string>(message: string, options: T[], flag: string): Promise<T>;
 export declare function getPassword(message: string): Promise<string>;
 type HandleAskResponse = 'required' | 'use_default' | ((answer: string | undefined) => Promise<string>);
